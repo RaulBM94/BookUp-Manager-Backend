2ef9baec8d99979b7fdf2f0482f7abfa7590115a
RaulBM94 <enderbrumen@gmail.com>
2022-12-12 19:06:16 +0000

    INSTALLED: nodemailer CREATED: email controller and router

---
+++ BackEnd/api/controllers/emailer.controller.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+async function sendemail(req, res) {
+
+  let email = req.body.email
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: 'bookupmanager@gmail.com', // generated ethereal user
+      pass: 'vtptkfsyjqqpiipw', // generated ethereal password
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"BookUpManager" <bookupmanager@gmail.com>', // sender address
+    to: email, // list of receivers
+    subject: "Nueva funcionalidad de BookUpManager ✔", // Subject line
+    text: "TEXT: BookUpManager se congratula de anunciar que la funcionalidad de envío de mensajes ya está operativa  👏👏👏", // plain text body
+    html: "<b>HTML: BookUpManager se congratula de anunciar que la funcionalidad de envío de mensajes ya está operativa  👏👏👏</b>", // html body
+  });
+
+  console.log('Email sent')
+  res.json('received')
+
+}
+sendemail().catch(console.error);
+
+module.exports = {
+  sendemail
+};
--- BackEnd/api/routes/index.js
+++ BackEnd/api/routes/index.js
@@ -5,10 +5,13 @@ const restaurantRouter = require('./restaurant.router')
 const reservationRouter = require('./reservation.router')
 const tablesRouter = require ('./tables.router')
 const authRouter = require('./auth.router')
+const emailerRouter = require("./emailer.router.js");
+
 
 router.use('/users', usersRouter)
 router.use('/auth', authRouter)
 router.use('/restaurant', restaurantRouter)
 router.use('/reservation', reservationRouter)
 router.use('/tables', tablesRouter)
+router.use("/emailer", emailerRouter)
 module.exports = router;
