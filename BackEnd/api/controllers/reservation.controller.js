8b186a505c3cd75315c62a1ca8bd1cb5c06aefe8
RaulBM94 <enderbrumen@gmail.com>
2022-12-15 10:34:37 +0000

    FIXED: reservation controller

--- BackEnd/api/controllers/reservation.controller.js
+++ BackEnd/api/controllers/reservation.controller.js
@@ -1,40 +1,44 @@
-const ReservationModel= require('../models/reservation.model')
-const {handleError}=require('../utils')
+const ReservationModel = require('../models/reservation.model')
+const { handleError } = require('../utils')
 
-function getAllReservationsByQuery (req, res) {
-    console.log(req.params.search)
+function getAllReservationsByQuery(req, res) {
+    console.log(req.params)
+    console.log(req.query)
     ReservationModel
-        .find({date:req.params.search.date, shift:req.params.search.shift})
+        .find({
+            date: req.query.search.date,
+            shift: req.query.search.shift
+        })
         .then(response => res.json(response))
         .catch((err) => handleError(err, res))
 }
 
-function createReservation (req,res) {
+function createReservation(req, res) {
     console.log(req.body)
     ReservationModel
-    .create(req.body)
-    .then((user)=>res.json(user))
-    .catch((err)=>res.json(err))
+        .create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => res.json(err))
 }
 
-function updateReservation (req,res){
+function updateReservation(req, res) {
     ReservationModel
-    .findByIdAndUpdate(req.params.id,req.body,{
-        new:true,
-        runValidators: true
-    })
-    .then(response => res.json(response))
-    .catch((err) => handleError(err,res))
+        .findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        .then(response => res.json(response))
+        .catch((err) => handleError(err, res))
 }
 
-function deleteReservation (req,res){
+function deleteReservation(req, res) {
     ReservationModel
-    .findByIdAndDelete(req.params.id)
-    .then(response => res.json(response))
-    .catch((err) => handleError(err,res))
+        .findByIdAndDelete(req.params.id)
+        .then(response => res.json(response))
+        .catch((err) => handleError(err, res))
 }
 
-module.exports={
+module.exports = {
     getAllReservationsByQuery,
     createReservation,
     updateReservation,
